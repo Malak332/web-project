5b92376d55eef4da132ca4a259b5f1e776ad7e36
Malak332 <Malak.2024002484@bua.edu.eg>
2026-05-19 12:56:42 +0300

    Update script.java

--- script.java
+++ script.java
@@ -15,19 +15,4 @@ document.querySelectorAll(".nav-links a").forEach(link => {
     }
   });
 });
-const topBtn = document.getElementById("topBtn");
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 200) {
-    topBtn.style.display = "block";
-  } else {
-    topBtn.style.display = "none";
-  }
-});
-
-topBtn.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-});
